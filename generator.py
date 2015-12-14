47d088afdfce50d52d1d998cbb565e18524ef607
Niggin <rubik303@mail.ru>
2015-12-14 15:43:26 +0300

    Initial commit

---
+++ generator.py
@@ -0,0 +1,41 @@
+def generate_data(n=10, seed=0):
+    #n is the amount of peers in network
+    #bandwidth is matrix of downloading speed from computer i to computer j
+    #data is in mbps
+    import numpy as np
+    
+    if seed:
+        np.random.seed(seed)
+    
+    C = 1.0
+    
+    pos = np.random.randn(n, 2)
+    bandwidth = np.zeros((n, n))
+    for i in xrange(n):
+        for j in xrange(n):
+            if i != j:
+                bandwidth[i, j] = C / np.linalg.norm(pos[i] - pos[j]) #* (0.1 + np.random.random_sample())
+                
+    bound = np.max(bandwidth)
+    bandwidth = bandwidth / bound * 0.9
+    connection_types = [10.0, 50.0, 100.0]
+    upload_speed = np.zeros(n)
+    download_speed = np.zeros(n)
+    for i in xrange(n):
+        speed = connection_types[np.random.randint(0, len(connection_types))]
+        bandwidth[:, i] *= speed
+        upload_speed[i] = speed
+        download_speed[i] = speed
+    return np.around(bandwidth, 2), np.around(download_speed, 2), np.around(upload_speed)
+
+def prepare_data(bandwidth, download_speed, upload_speed, file_size, time_step):
+    import numpy as np
+    file_piece = file_size / time_step
+    if np.any(np.amax(bandwidth, axis=0) * time_step < 1):
+        raise Exception("There are peers that has a connection speed slower than accepted; increase the time step")
+    if 1 > time_step * np.min(np.eye(len(download_speed)) * 1e6 + bandwidth):
+        print Exception("Timestep is too small: there are connections that can't pass piece within the time step")
+        
+    return (bandwidth / time_step).astype(int), \
+            (download_speed / time_step).astype(int), \
+            (upload_speed / time_step).astype(int)
